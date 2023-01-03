8fd21f3b1b385c639456610443522f41547595cd
scorpion28 <z92k1t@gmail.com>
2023-01-03 15:28:39 +0200

    Code: Add ability to complete CRUD operations from IStoreRepository.cs model

--- SportsStore/Models/IStoreRepository.cs
+++ SportsStore/Models/IStoreRepository.cs
@@ -3,4 +3,8 @@
 public interface IStoreRepository
 {
     public IQueryable<Product> Products { get; }
+
+    void CreateProduct(Product p);
+    void SaveProduct(Product p);
+    void DeleteProduct(Product p);
 }
